f6e6cdcd48b1c493df38480690f5fec478a97b9c
zperez00 <rancid80s@csu.fullerton.edu>
2022-02-21 00:36:15 -0800

    Added skeleton classes for feature

--- app/src/main/java/fullerton/lfg/MainActivity.kt
+++ app/src/main/java/fullerton/lfg/MainActivity.kt
@@ -9,3 +9,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+
+
+class CreateAccount{
+    // Creates a user account for the app.
+    // requires the user to input an email and password
+    // to create the account.
+}
+
+class CreateLookingForGroup{
+    // User creates a looking for group event post.
+    // Information about the event is provided in this class.
+}
+
+class JoinLookingForGroup{
+    // User joins the looking for group event.
+}
