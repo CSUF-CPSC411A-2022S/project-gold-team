3f42656874354f155d41e3e5beb98282266dc0ab
Mle225 <Mle225@csu.fullerton.edu>
2022-03-10 12:44:42 +0700

    added UserInfo class

--- app/src/main/java/fullerton/lfg/MainActivity.kt
+++ app/src/main/java/fullerton/lfg/MainActivity.kt
@@ -9,3 +9,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+class UserInfo(var name: String, var age: Int, var address: String) {
+
+    private var _name = name
+    private var _age = age
+    private var _address = address
+    private var interests: List<String> = listOf()
+
+    fun getUserName(): String {
+        return _name
+    }
+
+    fun getUserAge(): Int {
+        return _age
+    }
+
+    fun getUserAddress() : String {
+        return _address
+    }
+
+    fun getUserInterests(): List<String> {
+        return interests
+    }
+
+}
