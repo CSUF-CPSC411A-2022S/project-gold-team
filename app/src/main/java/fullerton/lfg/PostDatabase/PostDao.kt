aee7e60fda1ed9bc2c617010828f33ae2ad00d6a
Joshua Konechy <Jkonechy@live.com>
2022-04-21 17:14:16 -0700

    Added the Database files for the RecyclerView database
    created and adapted the  post.kt, postDatabase.kt, and postDao

---
+++ app/src/main/java/fullerton/lfg/PostDatabase/PostDao.kt
@@ -0,0 +1,42 @@
+package fullerton.lfg.PostDatabase
+
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+/**
+ * Data access object for the Intersection entity. The class describes how data is
+ * stored, updated, retrieved, or deleted from the database.
+ */
+@Dao
+interface PostDao {
+    // Add an intersection entity to a table in the database.
+    // We use suspend to run the function asynchronously (coroutine).
+    @Insert
+    suspend fun insert(post: Post)
+
+    // Update an intersection entity to a table in the database. Often uses the primary key
+    // We use suspend to run the function asynchronously (coroutine).
+    @Update
+    suspend fun update(post: Post)
+
+    // Custom query for retrieving a single Intersection from a table in the database using
+    // its intersection id. We don't use suspend because LiveData objects are already designed
+    // to work asynchronous.
+    @Query("SELECT * from Post_table WHERE PostId = :key")
+    fun get(key: Long): LiveData<Post>?
+
+    // Custom query for retrieving all Intersection entities from a table in the database.
+    // Data is stored to a List LiveData. We don't use suspend because LiveData objects
+    // are already designed to work asynchronously.
+    @Query("SELECT * from Post_table ORDER BY PostId DESC")
+    fun getAllIntersections(): LiveData<List<Post>>
+
+    // Custom query for deleting all entities on a table in the database
+    // We use suspend to run the function asynchronously (coroutine).
+    @Query("DELETE from Post_table")
+    suspend fun clear()
+}
